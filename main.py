1b59e2208d69f2b7146e97fa57f1963fd3c05842
Maria Clara <mclaranb@icloud.com>
2025-10-24 16:59:03 -0300

    Atualiza bug

--- main.py
+++ main.py
@@ -14,43 +14,48 @@ client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 redirect_uri = 'http://127.0.0.1:8888/callback'
 scope = 'playlist-read-private'
 
-
-cache_handler = FlaskSessionCacheHandler(session)
-
-sp_oauth = SpotifyOAuth(
-    client_id=client_id,
-    client_secret=client_secret,
-    redirect_uri=redirect_uri,
-    scope=scope,
-    cache_handler=cache_handler,
-    show_dialog=True)
-
-sp = Spotify(auth_manager=sp_oauth)
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        scope=scope,
+        show_dialog=True
+    )
 
 @app.route('/')
 def home():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()): 
-        auth_url = sp_oauth.get_authorize_url() 
-        return redirect(auth_url)
+    if not session.get('token_info'):
+        return redirect(url_for('login'))
     return redirect(url_for('get_playlists'))
+
+@app.route('/login')
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
 
 @app.route('/callback')
 def callback():
-        sp_oauth.get_access_token(request.args['code]']) 
-        return redirect(url_for('get_playlists'))
-    
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session['token_info'] = token_info
+    return redirect(url_for('get_playlists'))
 
 @app.route('/get_playlists')
 def get_playlists():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
+    token_info = session.get('token_info')
+    if not token_info:
+        return redirect(url_for('login'))
 
+    sp = Spotify(auth=token_info['access_token'])
     playlists = sp.current_user_playlists()
-    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
-    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
 
-
+    playlists_html = '<h2>Suas playlists 🎶</h2><br>'
+    for pl in playlists['items']:
+        playlists_html += f'{pl["name"]}: <a href="{pl["external_urls"]["spotify"]}" target="_blank">{pl["external_urls"]["spotify"]}</a><br>'
     return playlists_html
 
 @app.route('/logout')
@@ -58,5 +63,5 @@ def logout():
     session.clear()
     return redirect(url_for('home'))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
